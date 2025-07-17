b61f642470c226e663e82a76c5f12b98a86661ba
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-07-17 20:44:09 +0200

    fixed a link error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,7 @@ export default function Footer() {
             <div className={styles.links}>
                 <a href="https://github.com/eleonoracappellesso" target="_blank" rel="noopener noreferrer">GitHub</a>
                 <a href="https://www.linkedin.com/in/eleonora-cappellesso-6499271b4/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                {/* Sostituisci tuoUsername con il tuo vero username! */}
-                <a href="https://www.instagram.com/tuoUsername" target="_blank" rel="noopener noreferrer">Instagram</a>
+                <a href="https://www.instagram.com/eleonora.cappellesso/" target="_blank" rel="noopener noreferrer">Instagram</a>
             </div>
             <p className={styles.copyright}>
                 © {new Date().getFullYear()} Eleonora Cappellesso. Tutti i diritti riservati.
