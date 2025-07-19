2d6f24482047bc56b7052378709ccb5719a9d19b
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-07-19 15:40:32 +0200

    modified text in projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,7 +6,7 @@ export const progetti = [
         type: "Progetto in Team", // Etichetta
         descrizione: "BoolBnB è la replica di una piattaforma per la ricerca e gestione di immobili in affitto, sviluppata in team con l'utilizzo di React per il frontend, Express per il backend e MySQL per la costruzione del database. Il design responsivo, realizzato con Bootstrap e CSS Modules, garantisce un'esperienza fluida e intuitiva su qualsiasi dispositivo. Le funzionalità principali includono: l'inserimento facile degli immobili da parte dei proprietari, con supporto per il caricamento di più immagini; una pagina di dettaglio per ogni immobile con carosello interattivo, recensioni e possibilità di contattare il proprietario; un'homepage con jumbotron, carosello degli immobili popolari e sezione di esplorazione tematica; infine una ricerca avanzata con filtri dettagliati senza necessità di ricaricare la pagina. La piattaforma è progettata per offrire una navigazione semplice e un design visivamente accattivante, rendendo la ricerca di alloggi facile e piacevole.",
         immagine: "/projects/BoolBnB.png",
-        technologies: ["React", "Express.js", "MySQL", "Bootstrap", "CSS Modules"]
+        technologies: ["Javascript", "React", "Express.js", "MySQL", "Bootstrap", "CSS Modules"]
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ export const progetti = [
         type: "Progetto Individuale",
         descrizione: "BoolBooks è una libreria online pensata per gli amanti della lettura, con un'interfaccia intuitiva e moderna. La homepage accoglie gli utenti con una sezione hero che introduce la piattaforma e include un button call-to-action, invitando alla scoperta del catalogo, un carosello interattivo che mostra una selezione di libri, offrendo un'anteprima delle opere disponibili. Ogni libro ha una pagina dedicata, dove gli utenti possono visualizzare dettagli, leggere recensioni e aggiungere le proprie opinioni per contribuire alla community. Lo stack tecnologico include MySQL per la gestione del database, Express.js per il backend e React per il frontend.Il design è sviluppato con Bootstrap e moduli CSS, garantendo un'interfaccia responsive e accessibile su dispositivi desktop, tablet e mobile. L'estetica si basa su una palette di colori rilassanti, creando un'esperienza visiva piacevole. L'usabilità è ottimizzata grazie a un layout fluido, animazioni leggere e un'interazione intuitiva, rendendo la scoperta di nuovi libri un'esperienza coinvolgente.",
         immagine: "/projects/BookShop.png",
-        technologies: ["React", "Express.js", "MySQL", "Bootstrap"]
+        technologies: ["Javascript", "React", "Express.js", "MySQL", "Bootstrap", "CSS Modules"]
     },
     {
         id: 3,
@@ -24,7 +24,7 @@ export const progetti = [
         type: "Progetto Individuale",
         descrizione: "Replica della nota piattaforma di streaming Netflix, con caricamento dinamico dei contenuti, sviluppata con Express per il backend e React per il frontend.L'integrazione con TMDB API permette di gestire cataloghi di film e serie TV in tempo reale, offrendo agli utenti un'esperienza sempre aggiornata. L'interfaccia è progettata con un design responsive, garantendo una navigazione ottimale su desktop, tablet e dispositivi mobili. Grazie all'utilizzo di CSS flessibile e componenti adattivi di React, il layout si adatta dinamicamente a diverse risoluzioni, mantenendo un'esperienza fluida e intuitiva su qualsiasi schermo.",
         immagine: "/projects/Netflix.png",
-        technologies: ["React", "Express.js", "TMDB API", "CSS"]
+        technologies: ["Javascript", "React", "Express.js", "REST API", "CSS"]
     },
     {
         id: 4,
@@ -44,6 +44,6 @@ export const progetti = [
         descrizione: `Il progetto web è una replica della homepage di Dropbox, sviluppata utilizzando HTML e CSS. L'obiettivo principale è riprodurre il layout e lo stile del sito originale, concentrandosi sulla fedeltà visiva e sulla responsività del design.​ La pagina presenta una struttura simile a quella di Dropbox, con sezioni dedicate alle funzionalità, ai piani e prezzi, nonché testimonianze di clienti.Include anche elementi interattivi come pulsanti per iniziare una prova gratuita o contattare l'ufficio vendite. Questo progetto rappresenta un esercizio pratico per migliorare le competenze nello sviluppo front-end, focalizzandosi sull'uso di media query per l'adattamento a diversi dispositivi e sull'applicazione di convenzioni di naming come SUIT CSS.`,
         immagine: "/projects/Dropbox.png",
         link: "https://eleonoracappellesso.github.io/htmlcss-dropbox/",
-        technologies: ["HTML", "CSS", "SUIT CSS"]
+        technologies: ["HTML", "CSS"]
     }
 ];
